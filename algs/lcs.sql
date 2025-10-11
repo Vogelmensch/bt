c8254db6b8ccc8747898aa0b54c394b091498523
David <davidknoepp@protonmail.com>
2025-10-11 18:05:15 +0200

    changed backtracking to use only one query

--- algs/lcs.sql
+++ algs/lcs.sql
@@ -1,7 +1,7 @@
 -- see https://en.wikipedia.org/wiki/Longest_common_subsequence#Worked_example
 
-CREATE OR REPLACE MACRO s1() AS 'das ist ein text';
-CREATE OR REPLACE MACRO s2() AS 'das ist ebenfalls ein text :)';
+CREATE OR REPLACE MACRO s1() AS 'das ist ein schöner text.';
+CREATE OR REPLACE MACRO s2() AS 'dies ist ein toller text.';
 
 CREATE OR REPLACE TABLE letters(xsym, xidx, ysym, yidx) AS (
     SELECT s1()[m], m, s2()[n], n
@@ -50,7 +50,7 @@ WITH RECURSIVE lcs (
         nxt.xsym, nxt.xidx,
         nxt.ysym, nxt.yidx,
         -- TODO: add support for both directions
-        greatest(l.len, u.len), IF(l.len = u.len, 'b', IF(l.len < u.len, 'u', 'l'))
+        greatest(l.len, u.len), IF(l.len < u.len, 'u', 'l') -- 'l' can also mean 'both directions'
     FROM 
         letters AS nxt JOIN 
         recurring.lcs AS l ON nxt.xidx = l.xidx+1 and nxt.yidx = l.yidx JOIN
@@ -62,37 +62,18 @@ WITH RECURSIVE lcs (
         nxt.xsym != nxt.ysym
     )
 ),
-backtrack (word, xidx, yidx) AS (
+backtrack (word, xidx, yidx) USING KEY (xidx, yidx) AS (
     SELECT '', length(s1()), length(s2())
 
-    UNION ALL
+    UNION 
 
-    (
     SELECT 
-        lcs.xsym || b.word,
-        b.xidx - 1,
-        b.yidx - 1
+        IF(dir = 'd', lcs.xsym || b.word, b.word),
+        IF(dir = 'u', b.xidx, b.xidx-1),
+        IF(dir = 'l', b.yidx, b.yidx-1)
     FROM 
         lcs  
         JOIN backtrack AS b ON lcs.xidx = b.xidx and lcs.yidx = b.yidx
-    WHERE dir = 'd'
-
-    UNION
-
-    SELECT b.word, b.xidx-1, b.yidx
-    FROM 
-        lcs  
-        JOIN backtrack AS b ON lcs.xidx = b.xidx and lcs.yidx = b.yidx
-    WHERE dir = 'l'
-
-    UNION
-
-    SELECT b.word, b.xidx, b.yidx-1
-    FROM 
-        lcs  
-        JOIN backtrack AS b ON lcs.xidx = b.xidx and lcs.yidx = b.yidx
-    WHERE dir = 'u' or dir = 'b'
-    )
 )
 SELECT word AS 'Longest Common Subsequence'
 FROM backtrack
