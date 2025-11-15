3f25903e1fa6f04f1943977f5894182db24183ed
David <davidknoepp@protonmail.com>
2025-11-15 14:42:34 +0100

    Implemented backtracking 🥳

--- queries/needleman-wunsch/using-key.sql
+++ queries/needleman-wunsch/using-key.sql
@@ -72,6 +72,96 @@ WITH RECURSIVE needleman_wunsch (
             coords AS c, 
             scores AS s
     )
+),
+backtrack(
+    x, y, 
+    string1,
+    string2) AS (
+    -- Initial Case
+    (
+        SELECT 
+            nw.xidx-1, nw.yidx,
+            l.xsym,
+            '-'
+        FROM 
+            needleman_wunsch AS nw JOIN 
+            letters AS l ON nw.xidx = l.xidx AND nw.yidx = l.yidx
+        WHERE
+            nw.xidx = length(s1()) AND 
+            nw.yidx = length(s2()) AND
+            nw.from_lft
+        
+        UNION
+
+        SELECT 
+            nw.xidx, nw.yidx-1,
+            '-',
+            l.ysym
+        FROM 
+            needleman_wunsch AS nw JOIN 
+            letters AS l ON nw.xidx = l.xidx AND nw.yidx = l.yidx
+        WHERE
+            nw.xidx = length(s1()) AND 
+            nw.yidx = length(s2()) AND
+            nw.from_up
+        
+        UNION
+
+        SELECT 
+            nw.xidx-1, nw.yidx-1,
+            l.xsym,
+            l.ysym
+        FROM 
+            needleman_wunsch AS nw JOIN 
+            letters AS l ON nw.xidx = l.xidx AND nw.yidx = l.yidx
+        WHERE
+            nw.xidx = length(s1()) AND 
+            nw.yidx = length(s2()) AND
+            nw.from_diag
+    )
+
+    UNION ALL 
+
+    (
+        SELECT 
+            nw.xidx-1, nw.yidx,
+            l.xsym || b.string1,
+            '-' || b.string2
+        FROM 
+            backtrack AS b JOIN 
+            needleman_wunsch AS nw ON b.x = nw.xidx AND b.y = nw.yidx JOIN 
+            letters AS l ON nw.xidx = l.xidx AND nw.yidx = l.yidx 
+        WHERE
+            nw.from_lft
+        
+        UNION
+
+        SELECT 
+            nw.xidx, nw.yidx-1,
+            '-' || b.string1,
+            l.ysym || b.string2
+        FROM 
+            backtrack AS b JOIN 
+            needleman_wunsch AS nw ON b.x = nw.xidx AND b.y = nw.yidx JOIN 
+            letters AS l ON nw.xidx = l.xidx AND nw.yidx = l.yidx 
+        WHERE
+            nw.from_up
+        
+        UNION
+
+        SELECT 
+            nw.xidx-1, nw.yidx-1,
+            l.xsym || b.string1,
+            l.ysym || b.string2
+        FROM 
+            backtrack AS b JOIN 
+            needleman_wunsch AS nw ON b.x = nw.xidx AND b.y = nw.yidx JOIN 
+            letters AS l ON nw.xidx = l.xidx AND nw.yidx = l.yidx 
+        WHERE
+            nw.from_diag
+    )
 )
 
-FROM needleman_wunsch;
+SELECT string1, string2
+FROM backtrack
+WHERE x = 0 AND y = 0;
