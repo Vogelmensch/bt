a02f81dcb5c890ba5eb301d268da419401651792
David <davidknoepp@protonmail.com>
2025-12-09 14:33:06 +0100

    First storage of time measurements implemented a week ago

--- lcs.py
+++ lcs.py
@@ -11,22 +11,21 @@ if __name__ == '__main__':
 
     parser.add_argument('-t', '--time', action='store_true', help='measure process time for query execution')
     parser.add_argument('-T', '--time_suppress_solution', action='store_true', help='measure time and suppress print of solution')
+    parser.add_argument('-f', '--file', type=str, help='measure time and store result in FILE')
 
+    parser.add_argument('-n', '--repeat', type=int, help='repeat REPEAT times')
+    
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-s', '--strings', type=str, nargs=2, help='String arguments to compare')
     group.add_argument('-r', '--random', type=int, nargs=2, help='use randomly generated strings of given lengths')
 
     args = parser.parse_args()
 
+    # Use provided strings
+    # If -r is being used, strings get assigned below
     if args.strings:
         string1 = args.strings[0]
         string2 = args.strings[1]
-    elif args.random:
-        string1 = generate(args.random[0])
-        string2 = generate(args.random[1])
-        if not args.time_suppress_solution:
-            print('String1: {}\nString2: {}'.format(string1, string2))
-            print()
 
     scripts = []
 
@@ -38,28 +37,59 @@ if __name__ == '__main__':
     if len(scripts) == 0:
         scripts.append('queries/lcs/using-key.sql')
 
-    for script in scripts:
-        script_name = script.split('/')[-1]
-        print(script_name) # Print script name
-        print('-' * len(script_name))
+    if args.file:
+        timer = Timer()
+        timer.connect(args.file, first_arg='length')
+    elif args.time or args.time_suppress_solution:
+        timer = Timer()
+    else:
+        timer = None
 
-        with open(script) as f:
-            query = f.read()
+    if args.repeat:
+        repeats = args.repeat
+    else:
+        repeats = 1
 
-        if args.time or args.time_suppress_solution:
-            timer = Timer()
-            timer.start()
+    for i in range(repeats):
+        print(f'Running queries {i+1}/{repeats}')
 
-        res = duckdb.sql(query.format(string1='\'' + string1 + '\'', string2='\'' + string2 + '\'')).fetchall()[0][0]
+        # Generate random strings
+        if args.random:
+            string1 = generate(args.random[0])
+            string2 = generate(args.random[1])
+            if not args.time_suppress_solution and not args.file:
+                print('String1: {}\nString2: {}'.format(string1, string2))
+                print()
 
-        if args.time or args.time_suppress_solution:
-            timer.stop()
+        # Run all queries with the same inputs
+        for script in scripts:
+            script_name = script.split('/')[-1]
+            
+            if not args.file:
+                print(script_name) # Print script name
+                print('-' * len(script_name))
 
-        if not args.time_suppress_solution:
-            for s in res:
-                print(s)
+            with open(script) as f:
+                query = f.read()
 
-        if args.time or args.time_suppress_solution:
-            timer.print_elapsed()
-        print()
-        print()
+            if timer:
+                timer.start()
+
+            res = duckdb.sql(query.format(string1='\'' + string1 + '\'', string2='\'' + string2 + '\'')).fetchall()[0][0]
+
+            if timer:
+                timer.stop()
+
+            if not args.time_suppress_solution and not args.file:
+                for s in res:
+                    print(s)
+
+            if args.time or args.time_suppress_solution:
+                timer.print_elapsed()
+
+            if args.file:
+                timer.store_time(script_name, len(string1))
+
+            if not args.file:
+                print()
+                print()
--- measure/time.py
+++ measure/time.py
@@ -1,8 +1,45 @@
 import time
+import duckdb
 
 class Timer:
     time_start = 0
     time_stop = 0
+    # Connection to database
+    con = None
+    table_name = None
+
+    # store current state in .db-file
+    def store_time(self, script_name, first_arg):
+        if not self.con or not self.table_name:
+            raise Error('Data storage failed. Did you create the storage file?')
+        
+        time_elapsed = self.time_stop - self.time_start
+        time_elapsed *= 1000
+
+        self.con.sql(
+            f'INSERT INTO {self.table_name} VALUES (\'{script_name}\', {first_arg}, {int(time_elapsed)})'
+        )
+
+    # prepare storage of results in .db-file
+    def connect(self, db_name, first_arg='first_arg', first_arg_type='INTEGER', table_name=None):
+        # connect if not connected yet
+        if not self.con:
+            self.con = duckdb.connect(db_name)
+
+        # create table
+        if not table_name:
+            now = time.localtime()
+            table_name = f'measurement_{time.strftime('%m%d_%H%M%S', now)}'
+            self.table_name = table_name
+        self.con.sql(
+            f'CREATE TABLE {table_name} (script_name TEXT, {first_arg} {first_arg_type}, time INTEGER)'
+        )
+
+    def close_connection(self):
+        if self.con:
+            self.con.close()
+        else:
+            print('Failed to close connection to database: No database connected.')
 
     def start(self):
         self.time_start = time.process_time()
