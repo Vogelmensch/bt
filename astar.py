fd01d166717493e18034a1446ca4e25aa65bff93
David <davidknoepp@protonmail.com>
2025-12-10 13:52:02 +0100

    measurement and storage in csv-format for astar

--- astar.py
+++ astar.py
@@ -1,25 +1,9 @@
 import duckdb
 import argparse
-from measure.time import Timer
+from measure.measure import Timer
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Perform A* query.', 
-        epilog='The graph needs to have the following schema: node_from: INT | node_to: INT | weight: INT')
-    parser.add_argument('db', type=str, help='path to .db-file holding the graph(s)')
-    parser.add_argument('graph', type=str, help='name of the graph to perform the query on')
-    parser.add_argument('start', type=int, help='id of the start node')
-    parser.add_argument('goal', type=int, help='id of the goal node')
-    parser.add_argument('heuristic', type=str, nargs='?', help='optional custom heuristic function. Default: h(x) = 0')
-
-    parser.add_argument('-u', '--using_key', action='store_true', help='USING KEY')
-    parser.add_argument('-c','--classic', action='store_true', help='use classic CTE')
-
-    parser.add_argument('-t', '--time', action='store_true', help='measure process time for query execution')
-    parser.add_argument('-T', '--time_suppress_solution', action='store_true', help='measure time and suppress print of solution')
-
-    args = parser.parse_args()
-
+# supply args from argparse
+def perform_query(args, timer):
     with duckdb.connect(args.db) as con:
         if args.heuristic:
             heuristic = args.heuristic
@@ -45,23 +29,71 @@ if __name__ == '__main__':
             with open(script) as f:
                 query = f.read()
 
-            if args.time or args.time_suppress_solution:
-                timer = Timer()
+            if args.time:
                 timer.start()
 
             res = con.sql(query.format(graph=args.graph, start_node=args.start, goal_node=args.goal, heuristic=heuristic)).fetchall()
 
             if args.time:
                 timer.stop()
+
+            if (len(res) > 0):
+                path, length = res[0]
+            else:
+                path, length = None, None
                 
-            if not args.time_suppress_solution:
+            if not args.suppress_solution:
                 if (len(res) == 0):
                     print('Nothing found.')
                 else:
-                    print('Path:\t {}'.format(res[0][0]))
-                    print('Length:\t {}'.format(res[0][1]))
+                    print('Path:\t {}'.format(path))
+                    print('Length:\t {}'.format(length))
 
-            if args.time or args.time_suppress_solution:
+            if args.time:
                 timer.print_elapsed()
+
+                # data to store in csv
+                if args.file != 'DONT STORE':
+                    data = [script_name, args.graph, path, length]
+                    timer.write_csv(data)
             print()
             print()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Perform A* query.', 
+        epilog='''
+        The graph needs to have the following schema: node_from: INT | node_to: INT | weight: INT
+
+        To perform multiple queries and store them all into one csv-file, provide -e / --extend. 
+        The provided strings extend the graph name.
+        ''')
+    parser.add_argument('db', type=str, help='path to .db-file holding the graph(s)')
+    parser.add_argument('graph', type=str, help='name of the graph to perform the query on')
+    parser.add_argument('start', type=int, help='id of the start node')
+    parser.add_argument('goal', type=int, help='id of the goal node')
+    parser.add_argument('heuristic', type=str, nargs='?', help='optional custom heuristic function. Default: h(x) = 0')
+
+    parser.add_argument('-u', '--using_key', action='store_true', help='USING KEY')
+    parser.add_argument('-c','--classic', action='store_true', help='use classic CTE')
+
+    parser.add_argument('-t', '--time', action='store_true', help='measure process time for query execution')
+    parser.add_argument('-x', '--suppress_solution', action='store_true', help='suppress print of solution')
+    # const if -f without FILE, default if not -f
+    parser.add_argument('-f', '--file', type=str, nargs='?', const='NOT PROVIDED', default='DONT STORE', help='measure time and store into FILE')
+
+    parser.add_argument('-e', '--extend', type=str, action='extend', nargs='*', help='extend graph name with provided strings and perform MULTIPLE queries')
+
+    args = parser.parse_args()
+
+
+    timer = Timer(args.file, header=['script','graph','path','length', 'time'])
+
+    if args.extend:
+        graph = args.graph
+        for extension in args.extend:
+            args.graph = graph + extension
+            perform_query(args, timer)
+    else:
+        perform_query(args, timer)
---
+++ measure/measure.py
@@ -0,0 +1,47 @@
+import csv
+import time
+
+class Timer:
+    def _generate_filename(self):
+        now = time.localtime()
+        return f'measure/measurement_{time.strftime('%m%d_%H%M%S', now)}.csv'
+
+    def __init__(self, filepath, header=None):
+        if filepath == 'NOT PROVIDED':
+            self.filepath = self._generate_filename()
+        else:
+            self.filepath = filepath
+
+        if header:
+            with open(self.filepath, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow(header)
+
+        self.time_start = 0
+        self.time_stop = 0
+
+    # Write data formatted as list and the current time
+    def write_csv(self, data: list):
+        time_elapsed = self.time_stop - self.time_start
+
+        with open(self.filepath, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(data + [time_elapsed])
+
+    def start(self):
+        self.time_start = time.process_time()
+
+    def stop(self):
+        self.time_stop = time.process_time()
+
+    def print_elapsed(self):
+        time_elapsed = self.time_stop - self.time_start
+        
+        # convert to convenient unit
+        if time_elapsed < 1:
+            time_elapsed *= 1000
+            unit = 'ms'
+        else:
+            unit = 's'
+
+        print('CPU time: {0:.2f} {1}'.format(time_elapsed, unit))
