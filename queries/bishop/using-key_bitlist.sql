d48312be7ae08b11651d1b9b71659367cdbb852c
David <davidknoepp@protonmail.com>
2026-03-15 16:04:12 +0100

    renamed bishop files (git got the tracking wrong)

--- queries/bishop/using-key_bitlist.sql
+++ queries/bishop/using-key_bitlist.sql
@@ -14,54 +14,56 @@
 CREATE OR REPLACE MACRO width() AS {width};
 CREATE OR REPLACE MACRO height() AS {height};
 
+CREATE OR REPLACE MACRO bitlist() AS {bitlist};
+
 WITH RECURSIVE bishop (
-    idx,
     x,          -- x and y coordinates defining the grid
     y, 
     sym_id,     -- id for symbol in picture
+    bitlist,    -- list of binary tuples, e.g. (10, 11, 01, 00, 10, ...)
     is_end      -- last cell gets a special character
 ) USING KEY (x, y) AS (
     -- ❶ Start in the center of the grid, with the entire bitlist
     SELECT 
-        0,
         (width()/2) :: INTEGER,
         (height()/2) :: INTEGER,
         1,
+        bitlist(),
         false
         
     UNION ALL
 
     (
     -- ❷ Calculate the next coordinates from the first element in bitlist. Pop this element from bitlist.
-    WITH new(idx, x, y, is_end) AS (
-        SELECT 
-        bishop.idx+1,
-        CASE 
-            WHEN bits.x = 0
-            THEN greatest(0, bishop.x-1)       -- don't move past borders
-            ELSE least(width()-1, bishop.x+1)
-        END,
-        CASE 
-            WHEN bits.y = 0
-            THEN greatest(0, bishop.y-1)       -- don't move past borders
-            ELSE least(height()-1, bishop.y+1)
-        END,
-        bishop.idx = (SELECT max(idx) FROM bits)             -- last element only
-    FROM bishop JOIN bits ON bishop.idx = bits.idx
+    WITH new(x, y, bitlist, is_end) AS (
+        SELECT
+            CASE 
+                WHEN bitlist[1][2] == '0' 
+                THEN greatest(0, x-1)       -- don't move past borders
+                ELSE least(width()-1, x+1)
+            END AS x,
+            CASE 
+                WHEN bitlist[1][1] == '0' 
+                THEN greatest(0, y-1)       -- don't move past borders
+                ELSE least(height()-1, y+1)
+            END AS y,
+            array_pop_front(bitlist),
+            length(bitlist) = 1             -- last element only
+        FROM bishop
+        WHERE length(bitlist) > 0           -- ❸ Repeat until the bitlist is empty
     )
     -- ❹ Select the new values and increate sym_id by one
     SELECT 
-        new.idx,
         new.x,
         new.y,
         -- if field has not been visited before, result is NULL
         coalesce(field_to.sym_id + 1, 1),
+        new.bitlist,
         new.is_end
     FROM 
         new 
         LEFT OUTER JOIN recurring.bishop AS field_to
-                        ON field_to.x = new.x and 
-                           field_to.y = new.y
+                        ON field_to.x = new.x AND field_to.y = new.y
    )
 )
 SELECT x, y, sym_id, is_end
